c25a381d19510635b513ad1a687dfe10682fc282
blitzolsen <ols17030@byui.edu>
2018-08-01 00:09:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/stettsen/compleat_andriod/ViewCookbook.java
@@ -0,0 +1,16 @@
+package com.example.stettsen.compleat_andriod;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewCookbook extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.view_cookbook, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/stettsen/compleat_andriod/ViewMakeNow.java
@@ -0,0 +1,16 @@
+package com.example.stettsen.compleat_andriod;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewMakeNow extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.view_make_now, container, false);
+        return rootView;
+    }
+}
